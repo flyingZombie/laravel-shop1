90fcee4f9956df8a50f65c4454f16e8846094c0f
Lester Li <Lester.shiyi.li@gmail.com>
2018-09-29 01:59:12 +0000

    basic layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+	<div class="container">
+		<p class="pull-left">
+			Coded by Lester
+		</p>
+		<p class="pull-right">
+			<a href="mailto:lester.shiyi.li@gmail.com?subject=feedback">email me</a>
+		</p>
+	</div>
+</footer>
