9d8e0f6daffb844fa4ceb80168a4e710ebdc2649
Lester Li <Lester.shiyi.li@gmail.com>
2018-10-11 01:12:44 +0000

    Backend management - user list

---
+++ app/Admin/Controllers/UsersController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\User;
+
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Content;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
+
+class UsersController extends Controller
+{
+    use ModelForm;
+
+    /**
+     * Index interface.
+     *
+     * @return Content
+     */
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+
+            $content->header('Users List');
+
+            $content->body($this->grid());
+        });
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        return Admin::grid(User::class, function (Grid $grid) {
+
+            $grid->id('ID')->sortable();
+            $grid->name('user name');
+            $grid->email('email');
+            $grid->email_verified('verified email')->display(function ($value) {
+                return $value?'Y':'N';
+            });
+
+            $grid->created_at('Created Time');
+            $grid->disableCreateButton();
+            $grid->actions(function ($actions) {
+                //$actions->disableView();
+                $actions->disableDelete();
+                $actions->disableEdit();
+            });
+            $grid->tools(function ($tools) {
+                $tools->batch(function ($batch) {
+                    $batch->disableDelete();
+                });
+            });
+        });
+    }
+}
