d768b09b831c2c17f797ff9ed7931f154274da15
Lester Li <Lester.shiyi.li@gmail.com>
2018-11-23 09:40:23 +0000

    import libraries for wechat and alipay

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Models\UserAddress;
 use App\Policies\UserAddressPolicy;
+use Monolog\Logger;
+use Yansongda\Pay\Pay;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,7 +27,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton('alipay', function ()
+        {
+            $config = config('pay.alipay');
+            if (app()->environment() !== 'product') {
+                $config['mode'] = 'dev';
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+            return Pay::alipay($config);
+        });
+
+        $this->app->singleton('wechat_pay', function ()
+        {
+            $config = config('pay.wechat');
+            if (app()->environment() !== 'production') {
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+            return Pay::wechat($config);
+        });
     }
 
     protected $policies = [
