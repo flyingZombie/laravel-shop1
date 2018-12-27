249016564f5a5444688f84b44af74da1b6894bc4
Lester Li <Lester.shiyi.li@gmail.com>
2018-12-27 00:27:03 +0000

    installments index page

---
+++ resources/views/installments/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+@section('title', 'Installments List')
+
+@section('content')
+
+    <div class="row">
+      <div class="col-lg-10 col-lg-offset-1">
+        <div class="panel panel-default">
+            <div class="panel-heading text-center">
+                <h2>Installments List</h2>
+            </div>
+            <div class="panel-body">
+                <table class="table table-bordered table-striped">
+                  <thead>
+                  <tr>
+                  <th>Ref. No</th>
+                      <th>Amount</th>
+                      <th>Period</th>
+                      <th>Rate</th>
+                      <th>Status</th>
+                      <th>Action</th>
+                  </tr>
+                  </thead>
+                    <tbody>
+                    @foreach($installments as $installment)
+                        <tr>
+                            <td>{{ $installment->no }}</td>
+                            <td>${{ $installment->total_amount }}</td>
+                            <td>{{ $installment->count }}</td>
+                            <td>{{ $installment->fee_rate }}%</td>
+                            <td>{{ \App\Models\Installment::$statusMap[$installment->status] }}</td>
+                            <td><a class="btn btn-primary btn-xs" href="">View</a></td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+                <div class="pull-right">{{ $installments->render() }}</div>
+            </div>
+        </div>
+      </div>
+    </div>
+@endsection
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,40 +1,40 @@
 <nav class="navbar navbar-default navbar-static-top">
-	<div class="container">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-				<span class="sr-only">Toggle Navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="{{ url('/')}}">
-				Laravel Shop
-			</a>
-		</div>
-		<div class="collapse navbar-collapse" id="app-navbar-collapse">
-			<ul class="nav navbar-nav">
-				@if(isset($categoryTree))
-					<li>
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-						  All Categories <b class="caret"></b>
-						</a>
-						<ul class="dropdown-menu multi-level">
-						  @each('layouts._category_item', $categoryTree, 'category')
-						</ul>
-					</li>
-				@endif
-			</ul>
-			<ul class="nav navbar-nav navbar-right">
-				@guest
-				  <li><a href=" {{ route('login')}} ">Login</a></li>
-				  <li><a href=" {{ route('register') }} ">Register</a></li>
-				@else
-				<li>
-					<a href="{{ route('cart.index') }}">
-						<span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>
-					</a>
-				</li>
-				    <li class="dropdown">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="{{ url('/')}}">
+                Laravel Shop
+            </a>
+        </div>
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <ul class="nav navbar-nav">
+                @if(isset($categoryTree))
+                    <li>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            All Categories <b class="caret"></b>
+                        </a>
+                        <ul class="dropdown-menu multi-level">
+                            @each('layouts._category_item', $categoryTree, 'category')
+                        </ul>
+                    </li>
+                @endif
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                @guest
+                    <li><a href=" {{ route('login')}} ">Login</a></li>
+                    <li><a href=" {{ route('register') }} ">Register</a></li>
+                @else
+                    <li>
+                        <a href="{{ route('cart.index') }}">
+                            <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>
+                        </a>
+                    </li>
+                    <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
                                 <img src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
@@ -43,15 +43,16 @@
                         </a>
                         <ul class="dropdown-menu" role="menu">
 
-                        	<li>
-                        		<a href=" {{ route('user_addresses.index') }}">Receive Address</a>
-                        	</li>
-                        	<li>
-                        		<a href="{{ route('orders.index') }}">My order</a>
-                        	</li>
+                            <li>
+                                <a href=" {{ route('user_addresses.index') }}">Receive Address</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('orders.index') }}">My order</a>
+                                <a href="{{ route('installments.index') }}">My installments</a>
+                            </li>
                             <li>
                                 <a href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
+                                   onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
                                     Log out
                                 </a>
@@ -61,9 +62,9 @@
                             </li>
                         </ul>
                     </li>
-				@endguest
-				
-			</ul>
-		</div>
-	</div>
+                @endguest
+
+            </ul>
+        </div>
+    </div>
 </nav>
